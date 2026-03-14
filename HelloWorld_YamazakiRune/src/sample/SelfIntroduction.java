642da1789a60e6df477e7a5f6439c72c14c4dfb6
ryama <ryama@192.168.1.7>
2026-03-14 17:09:51 +0900

    Java導入

---
+++ HelloWorld_YamazakiRune/src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		//氏名
+		String name ="山﨑 琉音";
+		//年齢
+		int age = 24;
+		//趣味
+		String hobby = "カフェ巡り";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		System.out.println("私の名前は"+ name + "です。");
+		System.out.println("年齢は"+ age +"歳です。");
+		System.out.println("趣味は"+ hobby +"です。");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
