37fc3ed3fd572dc1007c60889f16cc14454b349e
ryama <ryama@Driveline_21>
2026-03-26 18:55:44 +0900

    自己紹介プログラム 修正

--- HelloWorld_YamazakiRune/src/introduce/Main.java
+++ HelloWorld_YamazakiRune/src/introduce/Main.java
@@ -5,12 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
-
-		
-		person1.name = "鈴木太郎";
-		person1.age = 20;
-		person1.height = 1.7;
-		person1.weight = 60;
 		
 		person1.print();
 		System.out.println("合計"+ Person.count +"人です");
