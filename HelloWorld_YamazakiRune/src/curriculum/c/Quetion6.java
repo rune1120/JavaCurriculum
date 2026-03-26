d722b2b118c562125880c1812ef57dade1f564ac
ryama <ryama@Driveline_21>
2026-03-26 19:39:50 +0900

    Quetion6 修正

--- HelloWorld_YamazakiRune/src/curriculum/c/Quetion6.java
+++ HelloWorld_YamazakiRune/src/curriculum/c/Quetion6.java
@@ -4,28 +4,36 @@ import java.util.Scanner;
 
 public class Quetion6 {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-     Scanner scanner = new Scanner(System.in);
-     CPU cpuNumber = new CPU();
- 	   
-     String[] hands = {"グー", "チョキ", "パー"};
-     while (true) {
-    	 System.out.print("グー(0),チョキ(1),パー(2)を入力：");
-    	 int player = scanner.nextInt();
-    	 System.out.println("あなたの手："+ hands[player]);
-    	 int cpu = cpuNumber.getHand();
-    	 System.out.println("CPUの手："+ hands[cpu]);
-    	 
-    	 if (player == cpu) {
-    		 System.out.println("あいこ！もう1回");
-    	 } else if ((player + 2) %3 == cpu){
-    		 System.out.println("あなたの負け…");
-         } else {
-        	 System.out.println("あなたの勝ち！");
-        	 break;
-         }
-	} 
-     scanner.close();
+		Scanner scanner = new Scanner(System.in);
+		CPU cpuNumber = new CPU();
+
+		String[] hands = { "グー", "チョキ", "パー" };
+		while (true) {
+			System.out.print("グー(0),チョキ(1),パー(2)を入力：");
+			int player = scanner.nextInt();
+			if (player < 0 || player > 2) { //入力チェック
+				System.out.println("0~2を入力してください");
+				continue;
+			}
+
+			System.out.println("あなたの手：" + hands[player]);
+			int cpu = cpuNumber.getHand();
+			System.out.println("CPUの手：" + hands[cpu]);
+
+			if (player == cpu) {
+				System.out.println("あいこ！もう1回");
+			} else if ((player + 2) % 3 == cpu) {
+				System.out.println("あなたの負け…");
+			} else {
+				System.out.println("あなたの勝ち！");
+				break;
+			}
+		}
+		scanner.close();
 	}
-  }
+}
